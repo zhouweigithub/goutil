cad31a42a0200ee01b55a1c9841da4f6483e985d
zhouwei <zhouwei@xinqiwan.com>
2022-10-20 14:30:28 +0800

    update

--- src/main.go
+++ src/main.go
@@ -1,14 +1,5 @@
-package main
+package goutil
 
-import (
-	"fmt"
-	"time"
-
-	timeutil "github.com/zhouweigithub/goutil/timeUtil"
-)
-
-func main() {
-	fmt.Println(time.Now().Unix())
-	fmt.Println(time.Unix(1666236746, 0))
-	fmt.Println(timeutil.FormateDateTimeString(time.Unix(1666236746, 0)))
+func GetKey() string {
+	return "hello world"
 }
